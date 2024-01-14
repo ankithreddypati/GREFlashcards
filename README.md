c60e72b7e1f04d0b22ece160db3360645049918c
ankithreddypati <pati.a@northeastern.edu>
2024-01-14 08:54:40 -0500

    morewordsandbuildfix

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # GREFlashcards
+
+
+GreFlashcards is a vocabulary learning app designed for Pebble smartwatches for GRE examination. It provides an interactive and convenient way for users to enhance their vocabulary skills on the go. The app includes a curated set of vocabulary words commonly found in GRE exams, allowing users to practice and reinforce their knowledge at their own pace.
+
+## Key Features
+
+- **User-Friendly Interface:** GreVocab offers a simple and intuitive interface, making it easy for users to navigate and engage with the app effortlessly.
+
+- **Multiple Sets:** The app organizes vocabulary words into sets, allowing users to focus on specific topics or difficulty levels. Users can seamlessly switch between sets for a tailored learning experience.
+
+- **Word Details:** For each word, GreVocab displays its meaning, providing users with a comprehensive understanding of the vocabulary. Users can mark words as read or star them for future reference.
+
+- **Convenient Controls:** The app leverages Pebble's button controls for a seamless user experience. Users can easily navigate through sets, select words, and perform actions with just a few clicks.
+
+## Getting Started
+
+To use GREFlashcards on your Pebble smartwatch:
+
+1. Install the app on your Pebble device from rebble store.
+https://apps.rebble.io/en_US/application/65a3d950e46ef105779235d4
+2. Navigate through sets using the Pebble buttons.
+3. Select specific words to view their meanings.
+4. words are marked as read when viewed to track the progress .
+
+## Contributing
+
+I welcome contributions to GRE flashcards! If you have ideas for improvements, feature requests, or bug reports, feel free to [create an issue](https://github.com/ankithreddypati/grevocab/issues/new) or submit a pull request.
+
+## License
+
+GreVocab is licensed under the [MIT License](https://github.com/ankithreddypati/grevocab/blob/main/LICENSE). Feel free to use, modify, and distribute the app in accordance with the license terms.
+
+---
--- src/windows/sets.c
+++ src/windows/sets.c
@@ -65,76 +65,75 @@ static void initialize_sets() {
             all_sets[i].words[8] = (VocabWord){"audacious", "showing a willingness to take surprisingly bold risks. ", false};
             all_sets[i].words[9] = (VocabWord){"avaricious", "having or showing an extreme greed for wealth or material gain.", false};
         } else if (i == 4) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"banal", "so lacking in originality as to be obvious and boring.", false};
+            all_sets[i].words[1] = (VocabWord){"benign", "gentle and kind.", false};
+            all_sets[i].words[2] = (VocabWord){"brazen", "bold and without shame.", false};
+            all_sets[i].words[3] = (VocabWord){"calumny", "the making of false and defamatory statements about someone", false};
+            all_sets[i].words[4] = (VocabWord){"castigate", "reprimand (someone) severely.", false};
+            all_sets[i].words[5] = (VocabWord){"candid", "truthful and straightforward; frank.", false};
+            all_sets[i].words[6] = (VocabWord){"caustic", "sarcastic in a scathing and bitter way.", false};
+            all_sets[i].words[7] = (VocabWord){"construe", "interpret (a word or action) in a particular way.", false};
+            all_sets[i].words[8] = (VocabWord){"contrite", "feeling or expressing remorse at the recognition ", false};
+            all_sets[i].words[9] = (VocabWord){"convoluted", "extremely complex and difficult to follow..", false};
         } else if (i == 5) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"covet", "yearn to possess (something, especially something belonging to another)", false};
+            all_sets[i].words[1] = (VocabWord){"craven", "contemptibly lacking in courage; cowardly.", false};
+            all_sets[i].words[2] = (VocabWord){"decorum", "behaviour in keeping with good taste and propriety.", false};
+            all_sets[i].words[3] = (VocabWord){"deft", "neatly skilful and quick in one's movements.", false};
+            all_sets[i].words[4] = (VocabWord){"demur", "raise objections or show reluctance.", false};
+            all_sets[i].words[5] = (VocabWord){"derivative", "something which is based on another source.", false};
+            all_sets[i].words[6] = (VocabWord){"desiccate", "remove the moisture from (something); cause to become completely dry.", false};
+            all_sets[i].words[7] = (VocabWord){"diatribe", "a forceful and bitter verbal attack against someone or something.", false};
+            all_sets[i].words[8] = (VocabWord){"incredulous", "unwilling or unable to believe something. ", false};
+            all_sets[i].words[9] = (VocabWord){"ingenuous", "innocent and unsuspecting.", false};
         } else if (i == 6) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"betray", "unintentionally reveal; be evidence of.", false};
+            all_sets[i].words[1] = (VocabWord){"bucolic", "relating to the pleasant aspects of the countryside and country life.", false};
+            all_sets[i].words[2] = (VocabWord){"burgeon", "begin to grow or increase rapidly; flourish.", false};
+            all_sets[i].words[3] = (VocabWord){"cacophonous", "involving or producing a harsh, discordant mixture of sounds.", false};
+            all_sets[i].words[4] = (VocabWord){"canonize", "place in or regard as belonging to a canon of literary or artistic works", false};
+            all_sets[i].words[5] = (VocabWord){"censure", "express severe disapproval of (someone or something),", false};
+            all_sets[i].words[6] = (VocabWord){"chicanery", "the use of deception or subterfuge to achieve one's purpose.", false};
+            all_sets[i].words[7] = (VocabWord){"coalesce", "come together to form one mass or whole.", false};
+            all_sets[i].words[8] = (VocabWord){"cogent", "(of an argument or case) clear, logical, and convincing. ", false};
+            all_sets[i].words[9] = (VocabWord){"compelling", "evoking interest, attention, or admiration in a powerfully irresistible way..", false};
         } else if (i == 7) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"abate", "(of something unpleasant or severe) become less intense or widespread.", false};
+            all_sets[i].words[1] = (VocabWord){"abjure", "solemnly renounce (a belief, cause, or claim).", false};
+            all_sets[i].words[2] = (VocabWord){"anomalous", "deviating from what is standard, normal, or expected.", false};
+            all_sets[i].words[3] = (VocabWord){"antipathy", "a deep-seated feeling of aversion.", false};
+            all_sets[i].words[4] = (VocabWord){"arcane", "understood by few; mysterious or secret.", false};
+            all_sets[i].words[5] = (VocabWord){"arduous", "involving or requiring strenuous effort; difficult and tiring.", false};
+            all_sets[i].words[6] = (VocabWord){"artless", "without effort or pretentiousness; natural and simple.", false};
+            all_sets[i].words[7] = (VocabWord){"ascetic", "characterized by severe self-discipline and abstention", false};
+            all_sets[i].words[8] = (VocabWord){"assuage", "make (an unpleasant feeling) less intense. ", false};
+            all_sets[i].words[9] = (VocabWord){"contend", "compete with others in a struggle to achieve (something)..", false};
         } else if (i == 8) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"copious", "abundant in supply or quantity.", false};
+            all_sets[i].words[1] = (VocabWord){"cosmopolitan", "including people from many different countries.", false};
+            all_sets[i].words[2] = (VocabWord){"deference", "polite submission and respect.", false};
+            all_sets[i].words[3] = (VocabWord){"desultory", "lacking a plan, purpose, or enthusiasm.", false};
+            all_sets[i].words[4] = (VocabWord){"diffident", "modest or shy because of a lack of self-confidence.", false};
+            all_sets[i].words[5] = (VocabWord){"dilatory", "1. slow to act. 2. intended to cause delay.", false};
+            all_sets[i].words[6] = (VocabWord){"equivocate", "use ambiguous language so as to conceal the truth or avoid committing oneself.", false};
+            all_sets[i].words[7] = (VocabWord){"polarize", "divide or cause to divide into two sharply contrasting groups or sets of opinions or beliefs.", false};
+            all_sets[i].words[8] = (VocabWord){"prodigal", "having or giving something on a lavish scale. ", false};
+            all_sets[i].words[9] = (VocabWord){"verbose", "using or expressed in more words than are needed.", false};
         } else if (i == 9) {
-            all_sets[i].words[0] = (VocabWord){"didactic", "mamala", false};
-            all_sets[i].words[1] = (VocabWord){"disseminate", "mamala", false};
-            all_sets[i].words[2] = (VocabWord){"feasible", "mamala", false};
-            all_sets[i].words[3] = (VocabWord){"flout", "mamala", false};
-            all_sets[i].words[4] = (VocabWord){"homogeneous", "mamala", false};
-            all_sets[i].words[5] = (VocabWord){"humdrum", "mamala", false};
-            all_sets[i].words[6] = (VocabWord){"insipid", "mamala", false};
-            all_sets[i].words[7] = (VocabWord){"loquacious", "mamala", false};
-            all_sets[i].words[8] = (VocabWord){"misanthropic", "mamala ", false};
-            all_sets[i].words[9] = (VocabWord){"misnomer", "mamala.", false};
+            all_sets[i].words[0] = (VocabWord){"abhor", "regard with disgust and hatred.", false};
+            all_sets[i].words[1] = (VocabWord){"boisterous", "noisy, energetic, and cheerful.", false};
+            all_sets[i].words[2] = (VocabWord){"chivalrous", "(of a man or his behaviour) courteous and gallant,", false};
+            all_sets[i].words[3] = (VocabWord){"churlish", "rude in a mean-spirited and surly way.", false};
+            all_sets[i].words[4] = (VocabWord){"clandestine", "kept secret or done secretively, especially because illicit.", false};
+            all_sets[i].words[5] = (VocabWord){"complacent", "showing smug or uncritical satisfaction with oneself or one's achievements.", false};
+            all_sets[i].words[6] = (VocabWord){"cumbersome", "large or heavy and therefore difficult to carry or use; unwieldy.", false};
+            all_sets[i].words[7] = (VocabWord){"debilitating", "make (someone) very weak and infirm.", false};
+            all_sets[i].words[8] = (VocabWord){"deliberate", "done consciously and intentionally. ", false};
+            all_sets[i].words[9] = (VocabWord){"droll", "curious or unusual in a way that provokes dry amusement.", false};
         }
         
     }
 }
-
 
 
 
